c93429e731d5ff169a1a426d8655f4b5745d10f0
O. Qudah <69857856+BasicPixel@users.noreply.github.com>
2022-01-20 11:36:47 +0200

    Init commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# MD Wiki
+
+MD Wiki is a simple markdown-supported wikipedia-like website, made with Django for CS50's Web programming course (Week 3).
+
+## Usage
+
+To run this project locally, clone the repository, then run the following:
+
+```bash
+# Install dependancies
+pip install -r requirements.txt
+```
+
+Then migrate database changes:
+
+```bash
+# Migrate DB
+python manage.py migrate
+```
+
+Lastly start a local server:
+
+```bash
+# Install dependancies
+python manage.py runserver
+```
+
+---
---
+++ mail/static/mail/inbox.js
@@ -0,0 +1,69 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Use buttons to toggle between views
+  document
+    .querySelector("#inbox")
+    .addEventListener("click", () => load_mailbox("inbox"));
+  document
+    .querySelector("#sent")
+    .addEventListener("click", () => load_mailbox("sent"));
+  document
+    .querySelector("#archived")
+    .addEventListener("click", () => load_mailbox("archive"));
+  document.querySelector("#compose").addEventListener("click", compose_email);
+  document.querySelector("#compose-form").onsubmit = send_mail;
+
+  // By default, load the inbox
+  load_mailbox("inbox");
+});
+
+function compose_email() {
+  // Show compose view and hide other views
+  document.querySelector("#emails-view").style.display = "none";
+  document.querySelector("#compose-view").style.display = "block";
+
+  // Clear out composition fields
+  document.querySelector("#compose-recipients").value = "";
+  document.querySelector("#compose-subject").value = "";
+  document.querySelector("#compose-body").value = "";
+}
+
+function send_mail() {
+  const recipients = document.querySelector("#compose-recipients");
+  const subject = document.querySelector("#compose-subject");
+  const body = document.querySelector("#compose-body");
+
+  fetch("/emails", {
+    method: "POST",
+    body: JSON.stringify({
+      recipients: recipients.value,
+      subject: subject.value,
+      body: body.value,
+    }),
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      document.querySelector(
+        "#message"
+      ).innerHTML = `<blockquote>${error}</blockquote>`;
+    });
+  return false;
+}
+
+function load_mailbox(mailbox) {
+  // Show the mailbox and hide other views
+  document.querySelector("#emails-view").style.display = "block";
+  document.querySelector("#compose-view").style.display = "none";
+
+  // Show the mailbox name
+  document.querySelector("#emails-view").innerHTML = `<h3>${
+    mailbox.charAt(0).toUpperCase() + mailbox.slice(1)
+  }</h3>`;
+  fetch(`/emails/${mailbox}`)
+    .then((response) => response.json())
+    .then((emails) => {
+      emails.forEach();
+    });
+}
